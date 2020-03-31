1d5ca4ad8c6020d509d93223c0cedbe2f14be4dc
cficht <chris.ficht@gmail.com>
2020-03-31 13:53:20 -0700

    test and login route for auth

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,6 +2,7 @@ require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('auth routes', () => {
 
@@ -12,6 +13,29 @@ describe('auth routes', () => {
         username: 'Chris',
         password: 'abc123',
         profilePhotoUrl: 'http://https://placekitten.com/200/287'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'Chris',
+          profilePhotoUrl: 'http://https://placekitten.com/200/287',
+          __v: 0
+        });
+      });
+  });
+
+  it('login user', async() => {
+    await User.create({
+      username: 'Chris',
+      password: 'abc123',
+      profilePhotoUrl: 'http://https://placekitten.com/200/287'
+    });
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'Chris',
+        password: 'abc123'
       })
       .then(res => {
         expect(res.body).toEqual({
